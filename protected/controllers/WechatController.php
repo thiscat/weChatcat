a6a364b394ab96475caa887f7e678847d12c593b
thiscats <2230530480@qq.com>
2016-03-23 11:47:54 +0800

    add function responseMsg

--- protected/controllers/WechatController.php
+++ protected/controllers/WechatController.php
@@ -7,6 +7,35 @@ class WechatController extends Controller
 		$this->render('index');
 	}
 
+	//回复信息
+	public function responseMsg()
+	{
+		$postStr = $GLOBALS["HTTP_RAM_POST_DATA"];
+		if(!empty($postStr)){
+			$postObj = simplexml_load_string($postStr,'SimpleXMLElement',LIBXML_NOCDATA);
+			$toUsername = $postObj->ToUserName;
+			$fromUserName = $postObj->FromUserName;
+			$keyword = trim($postObj->Content);
+			$createTime = time();
+			$textTpl = "<xml>
+						<ToUserName><![CDATA[%s]]></ToUserName>
+						<FromUserName><![CDATA[%s]]></FromUserName>
+						<CreateTime>%s</CreateTime>
+						<MsgType><![CDATA[%s]]></MsgType>
+						<Content><![CDATA[%s]]></Content>
+						</xml>";
+			if($keyword == "?" || $keyword == "？"){
+				$msgType = "text";
+				$content = "当前时间".date('Y-m-d H:i:s',time());
+				$resultStr = sprintf($textTpl,$fromUserName,$toUsername,$createTime,$msgType,$content);
+				echo $resultStr;
+			}
+		}else{
+			echo "没有接收到数据";
+			exit;
+		}
+	}
+
 	//微信基本配置验证
 	public function actionValid()
 	{
