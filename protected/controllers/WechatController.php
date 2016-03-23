73a3f24a7e3b34a2bf6768c3a1b8eac02ddaac30
thiscats <2230530480@qq.com>
2016-03-23 15:40:29 +0800

    modify wechat

--- protected/controllers/WechatController.php
+++ protected/controllers/WechatController.php
@@ -14,30 +14,72 @@ class WechatController extends Controller
 	//回复信息
 	public function responseMsg()
 	{
-		$postStr = $GLOBALS["HTTP_RAM_POST_DATA"];
+		$postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
 		if(!empty($postStr)){
 			$postObj = simplexml_load_string($postStr,'SimpleXMLElement',LIBXML_NOCDATA);
-			$toUsername = $postObj->ToUserName;
-			$fromUserName = $postObj->FromUserName;
-			$keyword = trim($postObj->Content);
-			$createTime = time();
-			$textTpl = "<xml>
-						<ToUserName><![CDATA[%s]]></ToUserName>
-						<FromUserName><![CDATA[%s]]></FromUserName>
-						<CreateTime>%s</CreateTime>
-						<MsgType><![CDATA[%s]]></MsgType>
-						<Content><![CDATA[%s]]></Content>
-						</xml>";
-			if($keyword == "?" || $keyword == "？"){
-				$msgType = "text";
-				$content = "当前时间".date('Y-m-d H:i:s',time());
-				$resultStr = sprintf($textTpl,$fromUserName,$toUsername,$createTime,$msgType,$content);
-				echo $resultStr;
+			$msgType = trim($postObj->MsgType);
+
+			switch($msgType){
+				case "event":
+					$this->receiveEvent($postObj);
+					break;
+				case "text":
+					$this->receiveText($postObj);
 			}
 		}else{
 			echo "没有接收到数据";
 			exit;
 		}
+	}
+
+	/**
+	 * 触发事件
+	 * @object $postObj
+	 */
+	public function receiveEvent($postObj)
+	{
+		switch($postObj->Event){
+			case "subscribe":
+				$content = "关注信息";
+				break;
+		}
+		$this->transmit($postObj,$content);
+	}
+
+	/**
+	 * 发送文本信息
+	 * @obj $postObj
+	 */
+	public function receiveText($postObj)
+	{
+		$keyword = trim($postObj->Content);
+		if($keyword == "?" || $keyword == "？"){
+			$content = "当前时间".date('Y-m-d H:i:s',time());
+			$this->transmit($postObj,$content);
+		}
+	}
+
+	/**
+	 * 被动发送信息
+	 * @object $postObj 接收信息 对象形式
+	 * @string $content 发送信息的内容
+	 * @string string $msgType 信息类型，默认文本信息
+	 */
+	public function transmit($postObj,$content,$msgType = "text")
+	{
+		$toUsername = $postObj->ToUserName;
+		$fromUserName = $postObj->FromUserName;
+		$createTime = time();
+		$textTpl = "<xml>
+					<ToUserName><![CDATA[%s]]></ToUserName>
+					<FromUserName><![CDATA[%s]]></FromUserName>
+					<CreateTime>%s</CreateTime>
+					<MsgType><![CDATA[%s]]></MsgType>
+					<Content><![CDATA[%s]]></Content>
+					</xml>";
+
+		$resultStr = sprintf($textTpl,$fromUserName,$toUsername,$createTime,$msgType,$content);
+		echo $resultStr;
 	}
 
 	//微信基本配置验证
